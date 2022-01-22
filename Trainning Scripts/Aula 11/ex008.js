da49ba7ede81315144b57e2ab753b560687ec261
Thiago Bastos <85635824+thiagohbastos@users.noreply.github.com>
2022-01-22 17:08:24 -0300

    Inserting Files

---
+++ Trainning Scripts/Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 70
+console.log(`Sua velocidade atual é de ${velocidade}Km/h`)
+if(velocidade > 60){
+    console.log('Você está MULTADO por excesso de velocidade.')
+}
+console.log('Dirija sempre com cinto de segurança!')
